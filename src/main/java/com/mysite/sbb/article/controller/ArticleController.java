fb0ab42420523cc755df6981139dbb2fd718036c
xanqus <sbk98031816@gmail.com>
2022-07-06 10:51:15 +0900

    id에 따른 게ã시물 1건 조회

--- src/main/java/com/mysite/sbb/article/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -5,9 +5,11 @@ import com.mysite.sbb.article.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usr/article")
@@ -29,9 +31,9 @@ public class ArticleController {
 
     @RequestMapping("/detail")
     @ResponseBody
-    public Article showArticleDetail() {
-        return articleRepository.findById(1L).get();
-
+    public Article showArticleDetail(@RequestParam long id, String name) {
+        Optional<Article> article = articleRepository.findById(id);
+        return article.orElse(null);
     }
 
 
