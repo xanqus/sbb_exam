832f7339ee89018bd3bc7338bc71fa9ecc9fd82d
xanqus <sbk98031816@gmail.com>
2022-07-13 11:43:17 +0900

    점프투스프링부트, 19강, 질문 목록과 템플릿

--- src/main/java/com/mysite/sbb/Question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/Question/controller/QuestionController.java
@@ -2,23 +2,24 @@ package com.mysite.sbb.Question.controller;
 
 import com.mysite.sbb.Question.dao.QuestionRepository;
 import com.mysite.sbb.Question.domain.Question;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/question")
+@AllArgsConstructor
 public class QuestionController {
 
-    @Autowired
-    private QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
 
     @RequestMapping("/list")
-    @ResponseBody
-    public List<Question> showQuestions() {
-        return questionRepository.findAll();
+    public String showQuestions(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 }
